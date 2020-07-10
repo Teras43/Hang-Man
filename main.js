fca1d1403427aed728d9a0e55246550465ddf1d9
Teras43 <Brandcurtis43@gmail.com>
2020-07-09 18:39:15 -0600

    Added hints for the hangman game

---
+++ main.js
@@ -0,0 +1,17 @@
+const testWords = {
+    "Test": { hint: "A type of evaluation or assessment."},
+    "Hanged": { hint: "Attached and swinging from the neck."},
+    "Man": { hint: "A brother, husband, son, or guy."},
+    "Hanging": { hint: "Suspended by arms or other means by something."},
+    "Desk": { hint: "A table-like object you can place items on, usually a computer."},
+    "Monitor": { hint: "A display that is plugged into a computer."},
+    "Computer": { hint: "What you're playing this on right now."},
+    "Keyboard": { hint: "The object used to type."},
+    "Mouse": { hint: "Controller of the pointer on the monitor."},
+    "RGB": { hint: "Color code, usually gives strength and power to your computer."},
+    "Headset": { hint: "Object that sound comes out of, usually plugged into a computer."},
+    "Headphones": { hint: "Object that sound comes out of, usually plugged into a phone."},
+    "Vape": { hint: "Replacement of ciggarettes in the recent years, big clouds."},
+    "Video Games": { hint: "A game you play in a visual way, usually on a TV or monitor."},
+    "Phone": { hint: "Pocket computer."}
+}
